e8552b8b707f3609f96dd44a303e616a196adfda
JuniorPoligraphist <juniorpoligraphist@gmail.com>
2016-06-08 14:31:14 +0300

    My start. Created the base of project structure

---
+++ FlappyGame/desktop/src/com/juniorpoligraphist/flappygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juniorpoligraphist.flappygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juniorpoligraphist.flappygame.FlappyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyGame(), config);
+	}
+}
